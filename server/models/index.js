db93f3491e2a7b9010f16522045ef665717f96ff
DragoonKite <frank.depaolo@yahoo.com>
2021-05-27 17:01:33 -0500

    added typedef/resolvers for searching brewery data

--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,4 @@
 const User = require('./User');
+const Brewery = require('./Brewery')
 
-module.exports = { User };
+module.exports = { User, Brewery };
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -7,6 +7,24 @@ const typeDefs = gql`
         email: String
     }
 
+    type Brewery {
+        _id: ID
+        name: String
+        breweryType: String
+        street: String
+        address2: String
+        adress3: String
+        city: String
+        state: String
+        countyProvince: String
+        postalCode: String
+        country: String
+        longitude: String
+        latitude: String
+        phone: String
+        websiteUrl: String
+    }
+
     type Auth {
         token: ID!
         user: User
@@ -14,10 +32,15 @@ const typeDefs = gql`
 
     type Query {
         me: User
+        brewery(name: String!): Brewery
+        breweryCity(city: String!): [Brewery]
+        breweryType(breweryType: String!): [Brewery]
+        breweryState(state: String!): [Brewery]
     }
 
     type Mutation {
         addUser(username: String!, email: String!, password: String!): Auth
+        login(email: String!, password: String!): Auth
     }
 `;
 
