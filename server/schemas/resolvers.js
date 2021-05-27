1e683c244b78627286c05bb54a861f02a785d2e4
DragoonKite <frank.depaolo@yahoo.com>
2021-05-26 21:01:58 -0500

    got concurrent server working.

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,8 +1,30 @@
-
+const { User } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
-    Query: {},
-    Mutation: {}
+    Query: {
+        me: async (parent, args, context) => {
+            if(context.user) {
+                const userData = await User.findOne({})
+                .select('-__v -password')
+                .populate('thoughts')
+                .populate('friends');
+
+                return userData;
+            }
+
+            throw new AuthenticationError('Not logged in.')
+        }
+    },
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args)
+            const token = signToken(user)
+
+            return {token, user};
+        }
+    }
 };
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,24 @@
 const { gql } = require('apollo-server-express');
 
-const typeDefs = gql ``;
+const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        email: String
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Query {
+        me: User
+    }
+
+    type Mutation {
+        addUser(username: String!, email: String!, password: String!): Auth
+    }
+`;
 
 module.exports = typeDefs;
