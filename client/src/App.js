016898d6430dca8578fa4cc1460f314bf1c1779e
Mon <rocketorangemen@gmail.com>
2021-05-27 15:43:08 -0500

    user's lat and lon in console

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+let userIP = "";
+let userLat = 0;
+let userLon = 0;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
+      {/* <h1>{userLat}</h1> */}
     </div>
   );
 }
+
+function userLocation() {
+    fetch("https://api.ipify.org/?format=json").then(function(response) {
+        if(response.ok){
+            response.json().then(function(data){
+                userIP = data.ip 
+
+                //uses ip address to get physical location data
+                return fetch("https://ipapi.co/" + userIP + "/json")
+            }).then(function(response){
+                if(response.ok){
+                    response.json().then(function(data){
+                        //save location data for future use
+                        userLat = data.latitude;
+                        userLon = data.longitude;
+                        console.log(userLat, userLon);
+                    });
+                }
+            });
+        }
+    })
+};
+
+userLocation();
 
 export default App;
