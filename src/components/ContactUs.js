8a6bea432c31b65f8fbb1964b92b4aea19d22b6e
muncle <blueink38@yahoo.com>
2021-05-30 23:25:00 -0500

    added capture to contact form

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,67 +1,69 @@
-import React from 'react'
-import { Form, Input, TextArea, Button, Select, Container, Segment } from 'semantic-ui-react'
+import React, { Component } from 'react'
+import { Form, Input, TextArea, Button, Container, Segment } from 'semantic-ui-react'
 
-const FormExampleFieldControlId = () => (
-  <div class="ui segment contactform" >
-
-  <Segment  inverted>
-  <h2 style={{textAlign: "center" ,color:'#f0f5f1'}}>Contact Us</h2>
-  <br></br>
-  <Form inverted>
-    <Container>
-    <Form.Group inverted >
-      <Form.Field
-        id='form-input-control-first-name'
-        control={Input}
-        label='First name'
-        placeholder='First name'
-        width={8}
-
-      />
-      <Form.Field
-        id='form-input-control-last-name'
-        control={Input}
-        label='Last name'
-        placeholder='Last name'
-        width={8}
-
-      />
-    </Form.Group>
-    <Form.Field
-      id='form-textarea-control-opinion'
-      control={TextArea}
-      label='What do you want us to know?'
-      placeholder='Tell us!'
-      width={16}
-
-    />
-    <Form.Field
-      id='form-input-control-error-email'
-      
-      control={Input}
-      label='Email'
-      placeholder='email@email.com'
-      width={16}
+class FormCaptureValues extends Component {
+  state = { firstName: '', lastName: '', email: '', message: '', submittedFirstName: '', submittedLastName: '', submittedEmail: '', submittedMessage: '' }
 
 
-      error={{
-        content: 'Please enter a valid email address',
-        pointing: 'below',
-      }}
-    />
-    <Form.Field
-      id='form-button-control-public'
-      control={Button}
-      style={{textAlign: "center" ,color:'#f0f5f1'}}
-      class="fluid ui inverted yellow button large"
-      content='Submit'
-      width={2}
-      // label='Submit'
-    />
-    </Container>
-  </Form>
-  </Segment>
-  </div> 
-)
+  handleSubmit = () => {
+    const { firstName, lastName, email, message } = this.state
 
-export default FormExampleFieldControlId
+    this.setState({ submittedFirstName: firstName, submittedLastName: lastName, submittedEmail: email, submittedMessage: message })
+  }
+
+  render() {
+    const { firstName, lastName, email, message, submittedFirstName, submittedLastName, submittedEmail, submittedMessage } = this.state
+
+    return (
+      <div class="ui segment contactform" >
+        <Segment inverted>
+          <h2 style={{textAlign: "center", color: '#f0f5f1'}}>Contact Us</h2>
+          <br></br>
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Group>
+            <Form.Input
+              placeholder='First name'
+              name='firstName'
+              value={firstName}
+              onSubmit={this.handleSubmit}
+            />
+            <Form.Input
+              placeholder='Last Name'
+              name='lastName'
+              value={lastName}
+              onSubmit={this.handleSubmit}
+            />
+            <Form.Group>
+            <Form.Input
+              placeholder='Email'
+              name='email'
+              value={email}
+              onChange={this.handleSubmit}
+            />
+            <Form.Input
+              placeholder='Message'
+              name='message'
+              value={message}
+              onChange={this.handleSubmit}
+            />
+            <Form.Button content='Submit'
+            control={Button}
+            style={{textAlign: "center", color: '#f0f5f1'}}
+            class="fluid ui inverted yellow button large"
+            content='submit'
+            width={2} />
+            </Form.Group>
+          </Form.Group>
+        <pre>{JSON.stringify({ firstName, lastName, email, message }, null,4 )}</pre>
+        <strong>onSubmit:</strong>
+        <pre>{JSON.stringify({ submittedFirstName, submittedLastName, submittedEmail, submittedMessage }, null, 4)}</pre>
+    )
+      </Form>
+    </Segment>
+  </div>
+    )
+  }
+  
+}
+
+export default FormCaptureValues
