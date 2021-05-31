ccc201c0d4f20febe0f3131294cfccbec84a0fbb
muncle <blueink38@yahoo.com>
2021-05-31 17:23:02 -0500

    cory changed some stuff

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React, { Component } from 'react';
 import 'semantic-ui-css/semantic.min.css';
 import Header from './components/Header';
-import About from './components/About';
-import Portfolio from './components/Portfolio';
+import FindBrewery from './components/FindBrewery';
+import Blip from './components/Blip';
 import Hero from './components/Hero';
-
 import ContactUs from './components/ContactUs';
 import Footer from './components/Footer';
-
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header/>
         <Hero/>
-        <Portfolio/>
-        <About/>   
+        <Blip/>
+        <FindBrewery/>   
         <ContactUs/>
         <Footer/>
       </div>
     );
   }
 }
-
 export default App;
