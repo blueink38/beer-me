6e78b6036255aa7e1d5e8a764459f31e3a48254a
DragoonKite <frank.depaolo@yahoo.com>
2021-05-31 14:16:54 -0500

    added mutations to client side

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -11,3 +11,61 @@ export const ADD_USER = gql`
     }
   }
 `;
+
+export const DELETE_USERS = gql `
+  mutation deleteUsers{
+    deleteUsers{
+      username
+    }
+  }
+`;
+
+export const ADD_BREWERY_TO_DB = gql `
+  mutation addBrewery($name: String!, $breweryID: Int!, $breweryType: String!, 
+    $street: String, $address2: String, $address3: String, 
+    $city: String, $state: String, $countyProvince: String, 
+    $postalCode: String, $country: String, $longitude: String, 
+    $latitude: String, $phone: String, $websiteUrl: String) {
+    addBrewery(name: $name, breweryID: $breweryID, breweryType: $breweryType, 
+      street: $street, address2: $address2, address3: $address3, 
+      city: $city, state: $state, countyProvince: $countyProvince, 
+      postalCode: $postalCode, country: $country, longitude: $longitude, 
+      latitude: $latitude, phone: $phone, websiteUrl: $websiteUrl) {
+        name
+        breweryID
+        breweryType
+        street
+        address2
+        address3
+        city
+        state
+        countyProvince
+        postalCode
+        country
+        longitude
+        latitude
+        phone
+        websiteUrl
+    }
+  }
+`;
+
+export const SAVE_BREWERY_TO_USER = gql `
+  mutation addSavedBrewery($brewId: ID!){
+    addSavedBrewery(brewId: $brewId){
+      breweries{
+        name
+      }
+    }
+  }
+`;
+
+export const REMOVE_BREWERY_FROM_USER = gql `
+  mutation removeSavedBrewery($brewId: ID!){
+    removeSavedBrewery(brewId: $brewId){
+      breweries{
+        name
+      }
+    }
+  }
+`;
