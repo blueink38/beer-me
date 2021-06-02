73bfa4b75dc2314fe9da7e071fe0db97c2fba420
DragoonKite <frank.depaolo@yahoo.com>
2021-06-01 20:00:40 -0500

    contact form now auto-clears on submit.

--- client/src/components/FindBrewery.js
+++ client/src/components/FindBrewery.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
+import SearchBreweries from './SearchBreweries'
+
 export default class FindBrewery extends Component {
   render() {
     return (
-<section id="about">
+      <section id="about">
          <div>
             <div id='brewfind'className="breweryfinder">
             <h1 style={{textAlign: "center", color:'#EBBA34'}}>Find A Brewery Near You!</h1>
             </div>
+            <SearchBreweries />
             </div>
             <br></br>
             <br></br>
