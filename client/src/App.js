41f7b987fb4c99dfe8a76e2b0688507f9dc51960
muncle <blueink38@yahoo.com>
2021-06-05 02:10:06 -0500

    contact form now send sends emails

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,8 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Home from './pages/Home';
 
+
+
 const client = new ApolloClient({
   request: (operation) => {
     const token = localStorage.getItem('id_token')
